7cbd1efadf84479572cb480606bc7821088736a7
veronj <veron_j@etna-alternance.net>
2019-02-25 17:08:33 +0100

    page proprietés

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @Route("/properties", name="property.index")
+     */
+    public function index()
+    {
+        return $this->render('property/index.html.twig', [
+            'controller_name' => 'PropertyController',
+            'current_menu' => 'properties'
+        ]);
+    }
+}
